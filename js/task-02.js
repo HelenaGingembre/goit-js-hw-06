94f99793c05694587b093eab30b090ae7e60fbbd
HelenaGingembre <e.izotovagingembre@outlook.com>
2022-07-31 16:40:55 +0300

    add task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,7 @@
+ 'use strict';
+//В HTML есть пустой список ul#ingredients.
+//Напиши скрипт, который для каждого элемента массива ingredients:
+
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -6,3 +10,21 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("#ingredients");
+
+const createElementList = (text) => {
+      //1) Создаст отдельный элемент <li>.
+      const itemEl = document.createElement("li");
+     //2) Добавит название ингредиента как его текстовое содержимое.
+      itemEl.textContent = text;
+      //3) Добавит элементу класс item.
+      itemEl.classList.add("item");
+      //console.log(itemEl);
+      return itemEl;
+}
+//4) После чего вставит все <li> за одну операцию в список ul#ingredients.
+list.prepend(...ingredients.map(ingredient => createElementList(ingredient)));
+
+
+
