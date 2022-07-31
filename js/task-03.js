b432645e8a492d709991d409efcf07ff6ddc6732
HelenaGingembre <e.izotovagingembre@outlook.com>
2022-08-01 00:35:51 +0300

    task-03.js +css gallery

--- js/task-03.js
+++ js/task-03.js
@@ -1,3 +1,6 @@
+//Напиши скрипт для создания галереи изображений 
+//по массиву данных.В HTML есть список ul.gallery.
+
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -12,3 +15,29 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+//Используй массив объектов images для создания элементов <img> вложенных в <li>.
+
+
+
+const listGallery = document.querySelector(".gallery");
+
+//1- Для создания разметки используй шаблонные строки и метод insertAdjacentHTML().
+const markup = images
+  .map((image) => `<li class="grid__item">
+  <img class="grid__img" src=${image.url} alt=${image.alt}></li>`)
+  .join("");
+
+//2- Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+listGallery.insertAdjacentHTML("beforeend", markup);
+listGallery.insertAdjacentHTML("beforebegin", "<h2 class='gallery__title'>NEW Gallery</h2>");
+
+//3- Добавь минимальное оформление галереи флексбоксами или гридами через 
+//CSS классы.
+listGallery.classList.add("grid");
+
+
+/*
+listGallery.style.display = "flex";
+listGallery.style.flexWrap = "wrap";
+listGallery.style.listStyle = "none";*/
