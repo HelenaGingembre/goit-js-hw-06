2c2306c5c9bb7ae3672358e2f133635d784071d9
HelenaGingembre <e.izotovagingembre@outlook.com>
2022-08-02 00:38:21 +0300

    task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -9,3 +9,29 @@
 //инпута становится зелёным, если неправильное - красным.
 // Для добавления стилей, используй CSS-классы valid и invalid,
 // которые мы уже добавили в исходные файлы задания.
+
+
+const input = document.querySelector("#validation-input");
+
+input.addEventListener('blur', onInputBlur);
+
+function onInputBlur(event) {
+            // console.log(' инпут потерял фокус событие blur');
+           
+    const inputCurrentValueLenght = event.currentTarget.value.length;
+               
+    if (inputCurrentValueLenght ===  Number(input.dataset.length)) {
+         input.classList.remove('valid');
+         input.classList.remove('invalid');
+    }
+    else if (inputCurrentValueLenght <  Number(input.dataset.length)) {
+        input.classList.remove('valid');
+        input.classList.add('invalid');
+    }
+    else {
+        input.classList.remove('invalid');
+        input.classList.add('valid');
+     }
+        
+    
+}
