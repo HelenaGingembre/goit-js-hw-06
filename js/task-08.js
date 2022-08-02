19cd1bab9db222171a3367a94aa9df8b3f8c612b
HelenaGingembre <e.izotovagingembre@outlook.com>
2022-08-02 15:58:20 +0300

    task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,13 +1,36 @@
 'use strict';
 //скрипт управления формой логина.
-// 1. Обработка отправки формы form.login-form должна быть по 
-//событию submit.
-// 2. При отправке формы страница не должна перезагружаться.
 // 3. Если в форме есть незаполненные поля, выводи alert с
 // предупреждением о том, что все поля должны быть заполнены.
-// 4. Если пользователь заполнил все поля и отправил форму,
+//4. Если пользователь заполнил все поля и отправил форму,
 // собери значения полей в обьект, где имя поля будет именем 
 // свойства, а значение поля - значением свойства.
-// Для доступа к элементам формы используй свойство elements.
-// 5. Выведи обьект с введенными данными в консоль и очисти значения 
-//полей формы методом reset.
+// 5. Выведи обьект с введенными данными в консоль
+ // и очисти значения полей формы методом reset.
+
+const form = document.querySelector('.login-form');
+
+// 1. Обработка отправки формы form.login-form должна быть по cобытию submit.
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    //2.При отправке формы страница не должна перезагружаться
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+
+    if (email === '' || password === '') {
+        return alert('Увага! Усі поля повинні бути заповнені.');
+    }
+   
+    const formData = new FormData(event.currentTarget);
+           
+    formData.forEach((value, name) => {
+                console.log(`${name}: ${value}`);
+            })
+    
+    event.currentTarget.reset(); // и очисти значения полей формы методом reset.
+}
+
